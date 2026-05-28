de35cad90d83f11221ece0df3f6294ba0035e876
EuniceNam <caellime@gmail.com>
2026-05-28 15:11:56 +0900

    add: Link로 교체, 클라이언트 컴포넌트로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Link from "next/link";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,19 +28,19 @@ export default function RootLayout({
       lang="ko"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="flex flex-col min-h-screen">
         <header>
           <nav className="flex">
-            <a href="/" className="p-2 rounded hover:bg-gray-100">
+            <Link href="/" className="p-2 rounded hover:bg-gray-100">
               메인
-            </a>
-            <a href="/posts" className="p-2 rounded hover:bg-gray-100">
+            </Link>
+            <Link href="/posts" className="p-2 rounded hover:bg-gray-100">
               글 목록
-            </a>
+            </Link>
           </nav>
         </header>
-        <main className="flex-grow">{children}</main>
-        <footer>푸터</footer>
+        <main className="flex-grow flex flex-col p-2">{children}</main>
+        <footer className="text-center p-2">푸터</footer>
       </body>
     </html>
   );
