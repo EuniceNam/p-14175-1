6511c6578c91790b45f0e2a5a28c1a77356dded6
EuniceNam <caellime@gmail.com>
2026-05-28 12:52:50 +0900

    feat: 메뉴 상단바

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,3 +1,12 @@
 export default function Page() {
-    return <div>Posts</div>;
+  return (
+    <>
+      <h1>글 목록</h1>
+
+      <ul>
+        <li>명언 1</li>
+        <li>명언 2</li>
+      </ul>
+    </>
+  );
 }
