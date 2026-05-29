dc83704dab482e0971199c0b828d782937e6e747
EuniceNam <caellime@gmail.com>
2026-05-29 14:21:51 +0900

    feat: 글 수정 페이지

---
+++ src/app/posts/[id]/edit/page.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { apiFetch } from "@/lib/backend/client";
+import { useParams, useRouter } from "next/navigation";
+
+export default function Page() {
+  const router = useRouter();
+
+  const { id: idStr } = useParams<{ id: string }>();
+  const id = Number(idStr);
+
+  const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const form = e.target as HTMLFormElement;
+
+    const titleInput = form.elements.namedItem("title") as HTMLInputElement;
+    const contentTextarea = form.elements.namedItem(
+      "content"
+    ) as HTMLTextAreaElement;
+
+    titleInput.value = titleInput.value.trim();
+
+    if (titleInput.value.length === 0) {
+      alert("제목을 입력해주세요.");
+      titleInput.focus();
+      return;
+    }
+
+    contentTextarea.value = contentTextarea.value.trim();
+
+    if (contentTextarea.value.length === 0) {
+      alert("내용을 입력해주세요.");
+      contentTextarea.focus();
+      return;
+    }
+
+    apiFetch(`/api/v1/posts/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        id: id,
+        title: titleInput.value,
+        content: contentTextarea.value,
+      }),
+    }).then((data) => {
+      alert(data.msg);
+
+      router.replace(`/posts/${id}`);
+    });
+  };
+
+  return (
+    <>
+      <h1>글 수정 페이지</h1>
+
+      <form className="flex flex-col gap-2 p-2" onSubmit={handleSubmit}>
+        <input
+          className="border p-2 rounded"
+          type="text"
+          name="title"
+          placeholder="새 제목"
+        />
+        <textarea
+          className="border p-2 rounded"
+          name="content"
+          placeholder="새 내용"
+        />
+        <div className="flex gap-2">
+          {/* <button
+            className="border p-2 rounded"
+            onClick={() => window.history.back()}
+          >
+            취소
+          </button> */}
+          <button className="border p-2 rounded" type="submit">
+            저장
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
